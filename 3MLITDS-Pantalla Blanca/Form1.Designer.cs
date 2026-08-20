e77e0ebff7fd802f37e9d9bec7ab867f0c6704f5
KrGiovani <KrGiovani@ARTORIASV>
2026-08-19 23:38:55 -0600

    Agregar archivos de proyecto.

---
+++ 3MLITDS-Pantalla Blanca/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace _3MLITDS_Pantalla_Blanca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnnombre = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.txbNombre = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnnombre
+            // 
+            this.btnnombre.Location = new System.Drawing.Point(63, 49);
+            this.btnnombre.Name = "btnnombre";
+            this.btnnombre.Size = new System.Drawing.Size(96, 45);
+            this.btnnombre.TabIndex = 0;
+            this.btnnombre.Text = "Nombre";
+            this.btnnombre.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(63, 175);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(97, 43);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Apellidos ";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // txbNombre
+            // 
+            this.txbNombre.Location = new System.Drawing.Point(180, 62);
+            this.txbNombre.Name = "txbNombre";
+            this.txbNombre.Size = new System.Drawing.Size(220, 20);
+            this.txbNombre.TabIndex = 2;
+            this.txbNombre.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(180, 187);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(220, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(106, 290);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 39);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Aceptar ";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(220, 290);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 39);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Cancelar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::_3MLITDS_Pantalla_Blanca.Properties.Resources._20359238;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txbNombre);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btnnombre);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Actividad01";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnnombre;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.TextBox txbNombre;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ 3MLITDS-Pantalla Blanca/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3MLITDS_Pantalla_Blanca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string nombre = btnnombre.Text;
+            MessageBox.Show("Hola bienvenido "+nombre, "Mensaje de inicio.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
